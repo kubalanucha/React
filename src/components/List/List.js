ee89cb5e3ddd4033bea41a33fb86614b1f0c388b
Kuba Lanucha <kubalanucha@gmail.com>
2023-01-13 13:04:40 +0100

    Fix styles

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,13 +3,15 @@ import styles from './List.module.scss';
 const List = () => {
   return (
     <div>
-      <header>
-        <h2>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
           Things to do<span> soon!</span>
         </h2>
       </header>
-      <p>Interesting things I want to check out</p>
-      <section>
+      <p className={styles.description}>
+        Interesting things I want to check out
+      </p>
+      <section className={styles.columns}>
         <article>
           <h2>Books</h2>
         </article>
