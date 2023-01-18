7f71f8d6e413ba06b4c84fa6e550d1c1cf6e1e62
Kuba Lanucha <kubalanucha@gmail.com>
2023-01-18 01:54:18 +0100

    Add new column via input

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,21 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
-  const columns = [
+  const [columns, setColumns] = useState([
     { id: 1, title: 'Books', icon: 'book' },
     { id: 2, title: 'Movies', icon: 'film' },
     { id: 3, title: 'Games', icon: 'gamepad' },
-    { id: 4, title: 'Dupa', icon: 'gamepad' },
-  ];
+  ]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setColumns([...columns, { id: shortid(), title: value }]);
+    setValue('');
+  };
+  const [value, setValue] = useState('');
   return (
     <div>
       <header className={styles.header}>
@@ -24,6 +32,15 @@ const List = () => {
           <Column key={column.id} title={column.title} icon={column.icon} />
         ))}
       </section>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button>Add column</button>
+      </form>
     </div>
   );
 };
